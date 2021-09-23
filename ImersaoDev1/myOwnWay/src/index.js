1f19d5ed5e91e8737a6ab9506c18b767fbcc0b3d
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-09-23 19:34:10 -0300

    FEITO JS dale

--- ImersaoDev1/myOwnWay/src/index.js
+++ ImersaoDev1/myOwnWay/src/index.js
@@ -1,1 +1,27 @@
-// ta funcionando :DD
+// função recebe input do usuario
+function recebeValores(nota1, nota2, nota3, nota4) {
+    return [parseFloat(document.querySelector(nota1).value), parseFloat(document.querySelector(nota2).value), parseFloat(document.querySelector(nota3).value), parseFloat(document.querySelector(nota4).value)]
+}
+
+// função calcula media
+function CalcularMedia() {
+    const soma = recebeValores(".PrimeiroTxfGrade", ".SegundoTxfGrade", ".TerceiroTxfGrade", ".QuartoTxfGrade")
+    let resultadoSoma = soma.reduce((total, currentElement) => total + currentElement) / 4
+    return parseFloat(resultadoSoma.toFixed(1))
+}
+
+// função clique botao
+function calculateButton() {
+    const button = document.getElementById("button")
+    button.addEventListener("click", MostrarResultado)
+}
+
+// função mostrar resultado botao
+function MostrarResultado() {
+    var label = document.querySelector(".formulario__label")
+    label.textContent = parseFloat(CalcularMedia())
+}
+
+document.addEventListener("DOMContentLoaded", calculateButton, false);
+
+console.log(CalcularMedia())
